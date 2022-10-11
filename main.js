488776796a1a1d52b86ad4f8eefdc7be36f58f61
jiawmei <jwmei@ucsc.edu>
2022-10-11 15:17:31 -0700

    gun/bullets

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ description = `
 
 characters = [
   `
-  llllll
-  ll l l
-  ll l l
-  llllll
-   c  c
-   c  c
-      `,
-    `
+  lllllllll 
+  lL
+  l 
+  
+  `,
+  `
   llllll
   ll l l
   ll l l
@@ -22,8 +20,8 @@ characters = [
 ];
 
 const gamesize = {
-  WIDTH: 125,
-  HEIGHT: 150
+  WIDTH: 100,
+  HEIGHT: 50
 };
 
 options = {
@@ -31,16 +29,49 @@ options = {
 };
 
 let player;
+let bullets;
+let movingDown = true;
 
 function update() {
   if (!ticks) {
     player = {
-      pos:vec(25, gamesize.HEIGHT * 0.9)
+      pos:vec(10, gamesize.HEIGHT * 0.5),
+      //bulletCD: 3,
+      firing: true
     };
+    bullets = [];
   }
 
   color("black");
   char("a", player.pos);
+
+  if(input.isJustPressed){
+    bullets.push({
+      pos:vec(player.pos.x, player.pos.y)
+    })
+  }
+  
+
+  bullets.forEach((b) => {
+    b.pos.x += 1;
+    color("yellow");
+    //bulletSpawn = b.pos + 5;
+    box(b.pos.x+5, b.pos.y, 1);
+
+  });
+  if(movingDown){
+    if((player.pos.y + 0.5) > 45){
+      movingDown = false;
+    }else{
+      player.pos.y += 0.5;
+    }
+  }else{
+    if(player.pos.y - 0.5 < 12.5){
+      movingDown = true;
+    }else{
+      player.pos.y -= 0.5;
+    }
+  }  
 }
 
 
